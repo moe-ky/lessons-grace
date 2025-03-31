5019aaf3501fe7a4ee996f7a5ac947b888dfe5d3
Yorubadgal <104074806+Yorubadgal@users.noreply.github.com>
2025-03-31 19:37:37 +0100

    add in guessing game

--- exercises/guessing_game.py
+++ exercises/guessing_game.py
@@ -5,3 +5,52 @@ the computer will pick a random number
 if you guess the number right, you get a point
 at then end of the 5 rounds, print how many points you have
 """
+# pseudocode
+""" in this guessing game the computer will be able choose a number with the use of a random library
+    and the user will have to guess the number with 5 chances at their disposal, using a while loop
+    the user will be asked for their guess with use of an input statement and
+    with the use of print statements, the user will be told they got the answers wrong
+    and when they guess the number right they have a point and the point is counted"""
+
+# computer has to generate a number between 1 to 10
+
+
+
+
+import random  # the library the comp  # uter uses to generate a random number
+def guessing_game():
+    user_score = 0
+    print("-" * 20)
+    print("Welcome to the number guessing game \n")
+    print("You have  five rounds \n")
+    print("-" * 20)
+    computer_num = random.randint(1, 10)
+
+    game_rounds = 0  # there are five rounds in this game
+    user_choice = input(
+        "Do you want to play the guessing game? (yes/no): ")
+    print(user_choice)
+    while game_rounds < 5:  # while zero is not equal zero
+        if user_choice.lower() == 'yes':  # if yes is not equal to yes, in this case i thought this means "no"
+            print("Let's go")
+            user_num = int(
+                input("Between 1 and 10, select a number of your choice: "))
+            print(user_num)
+            if user_num == computer_num:
+                print(f"You got the answer right")
+                user_score += 1
+            elif user_num > computer_num:
+                print(f"Your guess {
+                    user_num} is bigger than computer's guess, try again!")
+            else:
+                print(f"Your guess {
+                    user_num} is lesser than computer's guess, try again!")
+        else:
+            print("Game not started. Exiting.")
+            break
+        game_rounds += 1
+        print(computer_num)
+        print(user_score)
+
+
+guessing_game()
--- exercises/treasure_chest.py
+++ exercises/treasure_chest.py
@@ -9,4 +9,34 @@ Skills Tested:
 ✅ Using input() to get user input
 ✅ Using conditionals (if/else)
 ✅ Using a loop to limit attempts
-"""
+give the user a hint any of their numbers 
+being present in the computer generated numbers
+when they have given a wrong number"""
+
+
+import random
+# code = str(random.randint(100, 999))  # generating a three digit code
+code = str(456)  # generating a three digit code
+
+
+def treasure_chest():
+    print(f"Welcome to the treasure chest, where you guess to unlock the chest")
+    attempts = 0
+    while attempts < 3:
+        guess = input("Enter your three digit guess here: ")
+        if guess == code:
+            print("Congratulations! You've unlocked the treasure!")
+            break
+        else:
+            print("Wrong code. Try again.")
+            attempts += 1
+            for i in guess:
+                if i in code:
+                    print(i)
+                    print(f"")
+
+    print(f"You've used all your attempts. The chest will remain locked forever. The code was {
+        code}.")
+
+
+treasure_chest()
